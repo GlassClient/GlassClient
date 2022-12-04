3799fc5dd71ada180b1aa5e9dd8c5fd647843aca
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-12-04 13:31:03 +0000

    cleanup

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/mods/DamageIndicators.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/mods/DamageIndicators.kt
@@ -30,11 +30,11 @@ import kotlin.math.abs
 class DamageIndicators : Module() {
     private val aliveTicksValue = IntegerValue("AliveTicks", 20, 10, 50)
     private val sizeValue = IntegerValue("Size", 3, 1, 7)
-    private val colourValue = ListValue("ColourType", arrayOf("Green", "Custom", "Rainbow"), "None")
+    private val colourValue = ListValue("ColourType", arrayOf("Red", "Custom", "Rainbow"), "None")
     private val colorRedValue = IntegerValue("Red", 68, 0, 255).displayable { colourValue.get().equals("Custom") }
     private val colorGreenValue = IntegerValue("Green", 117, 0, 255).displayable { colourValue.get().equals("Custom") }
     private val colorBlueValue = IntegerValue("Blue", 255, 0, 255).displayable { colourValue.get().equals("Custom") }
-    private val colorRainbowValue = BoolValue("Rainbow", false).displayable { colourValue.get().equals("Rainbow") }
+    //private val colorRainbowValue = colourValue.get().equals("Rainbow")
     private val colorAlphaValue = IntegerValue("Alpha", 100, 0, 255)
     private val shadowValue = ListValue("Mode", arrayOf("LB", "Default", "Autumn", "Outline", "None"), "None")
 
@@ -51,7 +51,7 @@ class DamageIndicators : Module() {
                     if(lastHealth == entity.health) continue
                     if(lastHealth<entity.health){ return }
 
-                    val prefix = if (!colorRainbowValue.get()) (if(lastHealth>entity.health){"❤"}else{return}) else (if(lastHealth>entity.health){"-"}else{return})
+                    val prefix = if (!colourValue.get().equals("Rainbow")) (if(lastHealth>entity.health){"❤"}else{return}) else (if(lastHealth>entity.health){"-"}else{return})
                     particles.add(SingleParticle(prefix + BigDecimal(abs(lastHealth - entity.health).toDouble()).setScale(1, BigDecimal.ROUND_HALF_UP).toDouble()
                         ,entity.posX - 0.5 + Random(System.currentTimeMillis()).nextInt(5).toDouble() * 0.1
                         ,entity.entityBoundingBox.minY + (entity.entityBoundingBox.maxY - entity.entityBoundingBox.minY) / 2.0
@@ -92,7 +92,7 @@ class DamageIndicators : Module() {
                 GlStateManager.rotate(renderManager.playerViewX, textY, 0.0f, 0.0f)
                 GlStateManager.scale(-size, -size, size)
                 GL11.glDepthMask(false)
-                val doge = if (colourValue.get().equals("Green") ) "§c" else ""
+                val doge = if (colourValue.get().equals("Red") ) "§c" else ""
                 val x = -(mc.fontRendererObj.getStringWidth(particle.str) / 2)
                 val y = -(mc.fontRendererObj.FONT_HEIGHT - 1)
                 when (shadowValue.get()) {
@@ -106,7 +106,7 @@ class DamageIndicators : Module() {
                         mc.fontRendererObj.drawString(particle.str, x - 0.5F, y + 0.5F, Color(0, 0, 0, 130).rgb, false)
                     }
                 }
-                mc.fontRendererObj.drawString(doge + particle.str, (-(mc.fontRendererObj.getStringWidth(particle.str) / 2)), (-(mc.fontRendererObj.FONT_HEIGHT - 1)), (if (colorRainbowValue.get()) ColorUtils.rainbowWithAlpha(colorAlphaValue.get()) else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), colorAlphaValue.get())).rgb)
+                mc.fontRendererObj.drawString(doge + particle.str, (-(mc.fontRendererObj.getStringWidth(particle.str) / 2)), (-(mc.fontRendererObj.FONT_HEIGHT - 1)), (if (colourValue.get().equals("Rainbow")) ColorUtils.rainbowWithAlpha(colorAlphaValue.get()) else Color(colorRedValue.get(), colorGreenValue.get(), colorBlueValue.get(), colorAlphaValue.get())).rgb)
                 GL11.glColor4f(187.0f, 255.0f, 255.0f, 1.0f)
                 GL11.glDepthMask(true)
                 GlStateManager.doPolygonOffset(1.0f, 1500000.0f)
